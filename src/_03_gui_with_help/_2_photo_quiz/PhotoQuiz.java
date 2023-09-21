31344da54a71bf54ddb8bfa64d01778b7ae98537
student <student@192.168.1.17>
2023-09-21 13:56:07 -0700

    hi

--- src/_03_gui_with_help/_2_photo_quiz/PhotoQuiz.java
+++ src/_03_gui_with_help/_2_photo_quiz/PhotoQuiz.java
@@ -28,35 +28,55 @@ public class PhotoQuiz {
 		// “Copy Image Address” )
 
 		// 2. create a variable of type "Component" that will hold your image
-
+		Component comps;
+int score = 0;
 		// 3. use the "createImage()" method below to initialize your Component
-
+		comps = createImage("https://worldanimalfoundation.org/wp-content/uploads/2022/12/Blue-Nose-Pitbull-review.jpg");
 		// 4. add the image to the quiz window
+		quizWindow.add(comps);
 
 		// 5. call the pack() method on the quiz window
-
+		quizWindow.pack();
 		// 6. ask a question that relates to the image
-
+		String question = JOptionPane.showInputDialog("What dog is this");
 		// 7. print "CORRECT" if the user gave the right answer
+		if (question.equalsIgnoreCase("pitbull")) {
+			JOptionPane.showMessageDialog(null, "Correct");
+			score += 1;
 
-		// 8. print "INCORRECT" if the answer is wrong
-
+			// 8. print "INCORRECT" if the answer is wrong
+		}else {
+			JOptionPane.showMessageDialog(null,"incorrect");
+		}
 		// 9. remove the component from the quiz window (you may not see the
 		// effect of this until step 12)
-
+		quizWindow.remove(comps);
 		// 10. find another image and create it (might take more than one line
 		// of code)
-
+		comps = createImage("https://images.saymedia-content.com/.image/t_share/MTk2ODcyMjgzNTY1MzM1ODEx/types-of-doberman-pinschers-dog-breed-information.png");
 		// 11. add the second image to the quiz window
-
+		quizWindow.add(comps);
 		// 12. pack the quiz window
-
+		quizWindow.pack();
 		// 13. ask another question
+		String question2 = JOptionPane.showInputDialog("What dog is this");
 
+		if (question.equalsIgnoreCase("dobermann")) {
+			JOptionPane.showMessageDialog(null, "Correct");
+			score += 1;
+		}
+		
 		// 14+ check answer, say if correct or incorrect, etc.
 
+	 else {
+		JOptionPane.showMessageDialog(null,"incorrect");}
+		
+		JOptionPane.showMessageDialog(null, "your score is: "+score);
+		
 	}
 
+	
+	
 	private Component createImage(String imageUrl) {
 		JLabel imageLabel = new JLabel();
 		URL url;
@@ -68,7 +88,7 @@ public class PhotoQuiz {
 			// TODO Auto-generated catch block
 			JOptionPane.showMessageDialog(null, "I can't find your image!!");
 		}
-		
+
 		return imageLabel;
 	}
 
--- src/_03_gui_with_help/_3_drum_kit/DrumKit.java
+++ src/_03_gui_with_help/_3_drum_kit/DrumKit.java
@@ -6,8 +6,10 @@ import java.net.URL;
 
 import javax.swing.Icon;
 import javax.swing.ImageIcon;
+import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
+import javax.swing.JPanel;
 
 import game_tools.Sound;
 
@@ -17,20 +19,20 @@ public class DrumKit implements MouseListener {
 
     public void run() {
         //  Make a JFrame variable and initialize it using "new JFrame()"
-
+JFrame frame = new JFrame();
         //  Make the frame visible and
         // set its default close operation to JFrame.EXIT_ON_CLOSE
-
+frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         //  Set the title of the frame
-
+frame.setTitle("drums");
         //  Make a JPanel variable and initialize it using "new JPanel().
-
+JPanel panel = new JPanel();
         //  Add the panel to the frame. (The panel is invisible.)
-
+frame.add(panel);
         //  Use the image of a drum provided ("snare.jpg") or
         //  download an image of a drum from the Internet and drop it into the
         //  Drum Kit recipe package.
-
+comps = createImage
         //  Put the name of the drum image file in a String variable.
 
         //  Edit the next line to use your String variable
